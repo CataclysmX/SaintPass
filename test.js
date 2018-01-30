112aa9a0c6a94f613bfcfca471335adb74892b17
mamathieu <mamathieu@users.noreply.github.com>
2018-01-30 14:11:22 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ const client = new Discord.Client();
 var request = require('request');
 var figlet = require('figlet');
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 i = 0;
 function execute(){
@@ -71,3 +74,6 @@ function execute(){
 
 //setInterval(execute,2000);
 setInterval(execute,60000);
+
+client.login('NDA0MzEyNDMxNzcxNTE2OTM4.DUUA2A.WkhEuO_5S_3hihh-fJlf1z3ZQRk');
+client.login(process.env.BOT_TOKEN);
